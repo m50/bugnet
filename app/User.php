cf9fa5408178d4ca95e2a975b838f876485f11b6
Marisa Clardy <mclardy@titanhq.com>
2019-10-07 19:03:05 -0500

    Setup Users

--- app/User.php
+++ app/User.php
@@ -10,32 +10,21 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'is_admin' => 'boolean'
+    ];
+
+    protected $attributes = [
+        'is_admin' => false,
     ];
 
     public function projects()
