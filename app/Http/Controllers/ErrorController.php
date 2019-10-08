3641a4ca89237a50624234e2088d3f9ce152d0bd
Marisa Clardy <mclardy@titanhq.com>
2019-10-07 22:07:40 -0500

    add middleware and block out error controller

--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -14,7 +14,8 @@ class ErrorController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:web')->only(['index', 'show']);
+        $this->middleware('auth:api')->only('store');
     }
     
     /**
@@ -23,16 +24,6 @@ class ErrorController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -55,40 +46,6 @@ class ErrorController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Error $error)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Error  $error
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Error $error)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Error  $error
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Error $error)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Error  $error
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Error $error)
     {
         //
     }
