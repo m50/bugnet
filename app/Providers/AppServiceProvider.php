08c6be13e41abced39e3dddd72419025e49464be
Marisa Clardy <mclardy@titanhq.com>
2019-10-10 21:23:02 -0500

    Add a bunch of validation rules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 use App\Providers\TelescopeServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('can', function ($attribute, $value, $parameters) {
+            [$ability, $arguments] = $parameters;
+            return auth()->user()->can($ability, $arguments);
+        });
+        Validator::replacer('can', function ($message, $attribute, $rule, $parameters) {
+            return __('validation.can', ['attribute' => $attribute]);
+        });
+
+        Validator::extend('contain_number', function ($attribute, $value, $parameters) {
+            return preg_match('/[0-9]/', $value);
+        });
+        Validator::replacer('contain_number', function ($message, $attribute, $rule, $parameters) {
+            return __('validation.password.number', ['attribute' => $attribute]);
+        });
+        Validator::extend('contain_upper_case', function ($attribute, $value, $parameters) {
+            return preg_match('/[A-Z]/', $value);
+        });
+        Validator::replacer('contain_upper_case', function ($message, $attribute, $rule, $parameters) {
+            return __('validation.password.upper_case', ['attribute' => $attribute]);
+        });
+        Validator::extend('contain_lower_case', function ($attribute, $value, $parameters) {
+            return preg_match('/[a-z]/', $value);
+        });
+        Validator::replacer('contain_lower_case', function ($message, $attribute, $rule, $parameters) {
+            return __('validation.password.lower_case', ['attribute' => $attribute]);
+        });
+        Validator::extend('contain_symbol', function ($attribute, $value, $parameters) {
+            return preg_match('/[!@$%&<>?\[\]{}]/', $value);
+        });
+        Validator::replacer('contain_symbol', function ($message, $attribute, $rule, $parameters) {
+            return __('validation.password.symbol', ['attribute' => $attribute]);
+        });
     }
 }
